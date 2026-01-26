e187da9e60384c5feb3003083585aa60b4479a8c
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-01-26 19:25:20 +0600

    Module 14 practice task completed

---
+++ Milestone-3/Module-14/Array.js
@@ -0,0 +1,44 @@
+// let fruits = ["apple", "banana", "mango","guava", "litchi"]
+// console.log(fruits[2])
+
+// fruits[1] = "jambura";
+// console.log(fruits) 
+
+
+// let destinations = ["Kuakata", "kaptai", "saint-martin"]
+// console.log(destinations)
+
+// destinations.push("Rangamati")
+// console.log(destinations)
+
+// destinations.push("Cox's Bazar");
+// destinations.push("Sundarban");
+// console.log(destinations)
+
+// destinations.pop()
+// console.log(destinations)
+
+
+
+// let books = ["Math", "Physics", "Chemistry", "Biology"]
+
+// if (books.includes("Biology")) {
+//     console.log("You can be a doctor")
+// }else{
+//     console.log("You can't become a doctor")
+// }
+
+// let a = [];
+// let b = "mango"
+// let c = 30
+
+// console.log(Array.isArray(a))
+// console.log(Array.isArray(b))
+// console.log(Array.isArray(c))
+
+
+let fruits = ["Apple", "Jack-fruit", "Coconut", "Banana","Litchi"]
+let books = ["Math", "Physics", "Chemistry", "Biology"]
+
+let concatArray = fruits.concat(books)
+console.log(concatArray)
