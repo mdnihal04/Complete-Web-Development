f401d16319f8d698f7636c36f9d143a508df35b0
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-01-30 20:25:45 +0600

    JS conseptual session updated

---
+++ Milestone-3/B13_milestone_3_conceptual_session_array_string_loop_object_2/4.array_loop_iterate.js
@@ -0,0 +1,12 @@
+let array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+// console.log(array.length);
+// Using traditional for loop
+// for (let i = 0; i < array.length; i += 1) {
+//   console.log(array[i]);
+// }
+
+// For of loop
+for (let number of array) {
+  console.log(number);
+}
