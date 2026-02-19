3b537755e9041dc2e8f1834a53470b608d55ac72
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-02-19 11:41:11 +0600

    Login page design and functionality completed

---
+++ Milestone-4/Module-24 (Bank Project)/Payoo Bank Project/script/login.js
@@ -0,0 +1,31 @@
+console.log("login Successful")
+
+
+document.getElementById("login-btn").addEventListener("click",function(){
+    // 1. get the mobile number from input
+    const numberInput = document.getElementById("input-number");
+    const contactNumber = numberInput.value;
+    console.log(contactNumber)
+
+    // 2. get the pin input
+
+    const pinNumber = document.getElementById("input-pin").value
+    console.log(pinNumber)
+    // 3. match pin and mobile number
+    
+    
+
+    if(contactNumber=="01741815523" && pinNumber=="2004"){
+        // 3-1. true ==> alert ==> homepage
+        alert("Login Success");
+        window.location.assign("/home.html");
+    }
+    else{
+        // 3-2 false ==> alert ==> return
+        alert("login failed")
+        return;
+
+    }
+
+
+})
