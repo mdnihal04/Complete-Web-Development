d8e83a87ee785d638b0247c5954ed9c1c68127a6
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-02-21 11:44:09 +0600

    thrive functionality added

--- Milestone-4/Dom-conceputal-2/scripts/script.js
+++ Milestone-4/Dom-conceputal-2/scripts/script.js
@@ -13,6 +13,7 @@ const strugglingFilterButton = document.getElementById("struggling-filter-button
 const allCardSection = document.getElementById("all-cards");
 const mainContainer = document.querySelector("main");
 
+const filterSection = document.getElementById("filtered-section")
 
 function calculateCount(){
     total.innerText = allCardSection.children.length;
@@ -36,5 +37,84 @@ function toggleStyle(id){
     const selected = document.getElementById(id);
     selected.classList.remove("bg-gray-300", "text-black");
     selected.classList.add("bg-black", "text-white");
+
+    if(id == "thriving-filter-button"){
+        allCardSection.classList.add("hidden");
+        filterSection.classList.remove("hidden");
+    }else if(id =="all-filter-button"){
+        allCardSection.classList.remove("hidden");
+        filterSection.classList.add("hidden")
+    }
     
+}
+
+
+mainContainer.addEventListener("click", function(event){
+    
+    if(event.target.classList.contains("thriving-btn")){
+        const parentNode = event.target.parentElement.parentElement;
+        const plantName = parentNode.querySelector(".plant-name").innerText;
+        const latinName = parentNode.querySelector(".latin-name").innerText;
+        const light = parentNode.querySelector(".light").innerText;
+        const water = parentNode.querySelector(".water").innerText;
+        let status = parentNode.querySelector(".status").innerText;
+        const notes = parentNode.querySelector(".notes").innerText;
+        status = parentNode.querySelector(".status").innerText = "Thrive";    
+        
+
+        const cardInfo = {
+            plantName,
+            latinName,
+            light,
+            water,
+            status,
+            notes
+        }
+
+        const plantExist = thrivingList.find(item => item.plantName == cardInfo.plantName);
+
+        if(!plantExist){
+            thrivingList.push(cardInfo);        
+        }
+        renderThriving();
+        }
+      
+})
+
+function renderThriving(){
+    filterSection.innerHTML = ""
+
+    for(let thrive of thrivingList){
+        
+        let div = document.createElement("div");
+        div.className = "card flex justify-between border p-8";
+        div.innerHTML = `
+        <div class="space-y-6">
+                    <!-- Part 1 -->
+                    <div>
+                        <p class="plant-name text-3xl mb-2.5">${thrive.plantName}</p>
+                        <p class="latin-name">${thrive.latinName}</p>
+                    </div>
+                    <!-- Part 2 -->
+                    <div class="flex gap-2">
+                        <p class="light bg-gray-200 px-5">${thrive.light}</p>
+                        <p class="water bg-gray-200 px-5">${thrive.water}</p>
+                    </div>
+                     <!-- Part3 -->
+                      <p class="status">${thrive.status}</p>
+                      <p class="notes">${thrive.notes}</p>
+                      <!-- part 4 -->
+                    <div class="flex gap-5">
+                        <button class="thriving-btn bg-green-200 px-4 py-2 cursor-pointer">Thrive</button>
+                        <button class="struggling-btn bg-red-200 px-4 py-2 cursor-pointer">Struggle</button>
+                    </div>
+                </div>
+                <!-- Card Right -->
+                <div>
+                    <button class="btn-delete bg-red-200 text-red-600 px-4 py-2 cursor-pointer">Delete</button>
+                </div>
+        `
+
+        filterSection.appendChild(div)
+    }
 }
